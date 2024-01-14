c298438a4aeeaebbbbedddbc582034d79f33d308
Daniil Myasnikow <den4ic2001@gmail.com>
2024-01-14 15:46:59 +0300

    added redis ceche and queue service

--- config/services.php
+++ config/services.php
@@ -31,4 +31,16 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'osu' => [
+        'base_url' => 'https://osu.ppy.sh/api/v2/',
+        'auth_url' => env('OSU_AUTH_URL', 'https://osu.ppy.sh/oauth/authorize'),
+        'client_id' => env('OSU_CLIENT_ID', ''),
+        'client_secret' => env('OSU_CLIENT_SECRET', ''),
+        'scopes' => env('OSU_SCOPES', 'public'),
+        'token' => env('OSU_TOKEN'),
+        'throttle_settings' => [
+            'attempt_count' => 190,
+            'time_out' => 60,
+        ]
+    ]
 ];
